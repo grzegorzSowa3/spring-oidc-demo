2eeaa976084d2f13b6eefe1a3b1cc339918fb167
Grzegorz Sowa <grzegorz.sowa@allegropay.pl>
2022-10-29 14:03:37 +0200

    fix displaying user login

--- src/main/java/pl/recompiled/springoidcdemo/UserEndpoint.java
+++ src/main/java/pl/recompiled/springoidcdemo/UserEndpoint.java
@@ -1,5 +1,6 @@
 package pl.recompiled.springoidcdemo;
 
+import org.springframework.security.core.AuthenticatedPrincipal;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,13 @@ import java.util.Map;
 public class UserEndpoint {
 
     @GetMapping("/user")
-    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
-        return Collections.singletonMap("name", principal.getAttribute("name"));
+    public Map<String, Object> user(@AuthenticationPrincipal AuthenticatedPrincipal principal) {
+        String name;
+        if (principal instanceof OAuth2User oAuth2User) {
+            name = oAuth2User.getAttribute("login");
+        } else {
+            name = principal.getName();
+        }
+        return Collections.singletonMap("name", name);
     }
 }
