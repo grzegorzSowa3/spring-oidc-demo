6671c25837719bb6f57fcbdfa8f31d390c506709
Grzegorz Sowa <grzegorz.sowa314@gmail.com>
2022-10-29 11:51:29 +0200

    initial commit

---
+++ src/main/java/pl/recompiled/springoidcdemo/SpringOidcDemoApplication.java
@@ -0,0 +1,13 @@
+package pl.recompiled.springoidcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOidcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOidcDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/recompiled/springoidcdemo/SpringOidcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.recompiled.springoidcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOidcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
